3897bd67a9b87e540d7563ce551ab07972280c37
OndraG <ondra.to.stejne.necte@seznam.cz>
2017-07-23 20:04:34 +0200

    init commie

---
+++ src/main/java/cz/kedlub/ondra/App.java
@@ -0,0 +1,13 @@
+package cz.kedlub.ondra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
